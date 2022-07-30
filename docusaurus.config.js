06d776fe954d82f01dc17938cfcacab8ab0b9144
Dzung Do <dungdo@osd.vn>
2022-07-31 03:56:16 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,13 +30,8 @@ const config = {
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       {
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
         language: ["vi"],
-        // ```
       },
     ],
     require.resolve('docusaurus-plugin-image-zoom')
